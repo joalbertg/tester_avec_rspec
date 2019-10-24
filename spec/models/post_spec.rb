91450b5b8652e7377f98e0074bb1db605c98f193
Joalbert <joalbertgonzalez@gmail.com>
2019-10-23 21:00:59 -0500

    comment test was added and validates into comment model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe Post, type: :model do
   context 'comment association' do
     it 'should increment comments_count' do
       post = Post.create(name: 'Salut les gens')
-      post.comments << Comment.new(content: 'Demo')
-
+      post.comments << Comment.new(content: 'Demo 12345')
       expect(Comment.count).to eq(1)
       expect(post.comments_count).to eq(1)
     end
